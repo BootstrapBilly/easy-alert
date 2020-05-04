99b8efb1e013362028e74029f29d3c6f3f0c97c1
Billy <billyjcatchpole@gmail.com>
2020-05-04 12:16:04 +0100

    first commit

---
+++ Colours.js
@@ -0,0 +1,3 @@
+const colours = {red:"#DC143C", orange:"#ffa500", green:"#66CD00", blue:"#6495ED", middle_blue:"#4D86EB", dark_blue:"#3676e8", grey:"#778899", dark_grey: "#708090", poor:"#ff9b9b", fair:"#ffc04c", good:"#90EE90"};
+
+export default colours
